e358db2d79d27a56cfc7593917f7a3e768fe5228
AGoubs <arnaud@goubier.fr>
2021-08-11 11:38:12 +0200

    change engine 2

--- database/migrations/2021_08_03_102944_create_events_table.php
+++ database/migrations/2021_08_03_102944_create_events_table.php
@@ -14,6 +14,7 @@ class CreateEventsTable extends Migration
   public function up()
   {
     Schema::create('events', function (Blueprint $table) {
+      $table->engine = 'InnoDB';
       $table->id();
       $table->string('Nom');
       $table->date('Date');
