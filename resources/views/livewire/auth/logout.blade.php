64ac7a1118b20df2abd31bc84f9d752d8c348cf2
AGoubs <arnaud@goubier.fr>
2021-11-22 10:45:27 +0100

    Divider dropdown

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -41,6 +41,7 @@
         </div>
       </a>
     </li>
+    <hr class="horizontal dark my-2">
     <li>
       <li>
         <a class="dropdown-item border-radius-md" href="{{ route('change-password') }}">
@@ -54,6 +55,7 @@
         </a>
       </li>
       <li>
+        <hr class="horizontal dark my-2">
       <a class="dropdown-item border-radius-md" wire:click="logout">
         <div class="d-flex justify-content-between">
           <h6 class="text-sm font-weight-normal mb-0 p-2">
