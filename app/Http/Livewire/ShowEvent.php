982d9ab243d79f1becb36add5e9141c41bf109e1
AGoubs <arnaud@goubier.fr>
2021-11-23 11:46:52 +0100

    Get all users from event

--- app/Http/Livewire/ShowEvent.php
+++ app/Http/Livewire/ShowEvent.php
@@ -3,36 +3,43 @@
 namespace App\Http\Livewire;
 
 use App\Models\Event;
+use App\Models\EventByUser;
 use App\Models\Host;
+use App\Models\User;
 use Livewire\Component;
 
 class ShowEvent extends Component
 {
-    public $event;
-    public $eventId;
-    public $hosts;
-    public $tableField;
-    public $typeEvenement;
+  public $event;
+  public $eventId;
+  public $users = [];
+  public $hosts;
+  public $tableField;
+  public $typeEvenement;
 
-    public function render()
-    {
-        $this->typeEvenement = $this->event->type_event;
+  public function render()
+  {
+    $this->typeEvenement = $this->event->type_event;
 
-        return view('livewire.show-event');
+    $usersIds = EventByUser::where('event_id', $this->eventId)->pluck('user_id')->toArray();
+    foreach ($usersIds as $userId) {
+      $this->users[] = User::where('id',$userId)->first();
     }
+    return view('livewire.show-event');
+  }
 
-    public function mount()
-    {
-        if (isset($this->eventId)) {
-            $this->event = Event::find($this->eventId);
-        } else {
-            $this->event = Event::where('Date', date("Y-m-d"))->first();
-            if (!isset($this->event)) {
-                session()->flash('info',  "Pas d'évènement prévu aujourd'hui");
-                return redirect()->route('events.index');
-            } else {
-                $this->eventId = $this->event->id;
-            }
-        }
+  public function mount()
+  {
+    if (isset($this->eventId)) {
+      $this->event = Event::find($this->eventId);
+    } else {
+      $this->event = Event::where('Date', date("Y-m-d"))->first();
+      if (!isset($this->event)) {
+        session()->flash('info',  "Pas d'évènement prévu aujourd'hui");
+        return redirect()->route('events.index');
+      } else {
+        $this->eventId = $this->event->id;
+      }
     }
+  }
 }
