f20ee270bc98baa8f8730dc082750f4006781e5d
AGoubs <arnaud@goubier.fr>
2022-02-14 15:55:02 +0100

    Change disconnect button margin

--- resources/views/layouts/navbars/auth/nav-profile.blade.php
+++ resources/views/layouts/navbars/auth/nav-profile.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-main navbar-expand-lg bg-transparent shadow-none position-absolute px-0 w-100 z-index-2">
-  <div class="collapse navbar-collapse me-md-0 me-sm-4 mt-sm-0 mt-2" id="navbar">
+  <div class="collapse navbar-collapse me-3 mt-2" id="navbar">
     <div class="ms-auto pe-md-3 d-flex align-items-end">
       <livewire:auth.host-logout />
     </div>
