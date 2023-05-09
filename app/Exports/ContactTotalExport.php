8bbca9dea8542fd1a16f6264e8fa2003adab334e
AGoubs <arnaud@goubier.fr>
2023-05-09 15:39:23 +0200

    Remove excel's fields

--- app/Exports/ContactTotalExport.php
+++ app/Exports/ContactTotalExport.php
@@ -21,21 +21,21 @@ class ContactTotalExport implements FromCollection, WithHeadings, ShouldAutoSize
   public function headings(): array
   {
     return [
-      'Activité',
-      'Société',
+      // 'Activité',
+      // 'Société',
       'Nom',
       'Prénom',
       'Téléphone',
       'Email',
-      'Pays',
-      'Ville',
-      'Adresse',
-      'CP',
-      'Date de rendez-vous',
-      'Avec',
-      'N° SIRET',
+      // 'Pays',
+      // 'Ville',
+      // 'Adresse',
+      // 'CP',
+      // 'Date de rendez-vous',
+      // 'Avec',
+      // 'N° SIRET',
       'Commentaire',
-      'Pris par',
+      // 'Pris par',
     ];
   }
 
@@ -44,7 +44,9 @@ class ContactTotalExport implements FromCollection, WithHeadings, ShouldAutoSize
    */
   public function collection()
   {
-    return Contact::where('event_id', $this->eventId)->join('users', 'contacts.user_id', '=', 'users.id')->select('contacts.activity', 'contacts.company', 'contacts.name as contact_name', 'contacts.firstname', 'contacts.phone', 'contacts.email', 'contacts.country', 'contacts.city', 'contacts.address', 'contacts.postal',  'contacts.date_appointment', 'contacts.user_appointment', 'contacts.siret', 'contacts.comment', 'users.name')->get();  }
+    // return Contact::where('event_id', $this->eventId)->join('users', 'contacts.user_id', '=', 'users.id')->select('contacts.activity', 'contacts.company', 'contacts.name as contact_name', 'contacts.firstname', 'contacts.phone', 'contacts.email', 'contacts.country', 'contacts.city', 'contacts.address', 'contacts.postal',  'contacts.date_appointment', 'contacts.user_appointment', 'contacts.siret', 'contacts.comment', 'users.name')->get();
+    return Contact::where('event_id', $this->eventId)->join('users', 'contacts.user_id', '=', 'users.id')->select('contacts.name as contact_name', 'contacts.firstname', 'contacts.phone', 'contacts.email', 'contacts.comment')->get();
+  }
 
   public function title(): string
   {
